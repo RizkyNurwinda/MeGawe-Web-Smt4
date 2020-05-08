d80561347d3aff4fbde68c6c14ba253a9a9c90eb
nksari01 <58420977+nksari01@users.noreply.github.com>
2020-05-08 22:51:38 +0700

    Add files via upload

---
+++ application/views/home/head.php
@@ -0,0 +1,34 @@
+<head>
+  <title>MeGawe</title>
+  <!-- Template CSS -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="<?php echo base_url('asset/fonts/icomoon/style.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/bootstrap.min.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/magnific-popup.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/jquery-ui.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/owl.carousel.min.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/owl.theme.default.min.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/bootstrap-datepicker.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/animate.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/fonts/flaticon/font/flaticon.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/fl-bigmug-line.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/aos.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('asset/css/style.css');?>">
+
+  <!--CSS Koleksi Sendiri-->
+  <link rel="stylesheet" href="<?php echo base_url('asset/style2.css');?>">
+
+</head>
+  <?php $this->load->view("home/content");?>
+  <br>
+  <?php $this->load->view("home/isi");?>
+  <hr class="soft"/>
+  <?php $this->load->view("home/footer");?>
+  
+  
+    
+
+
+    
+    
---
+++ application/views/home/template.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php $this->load->view("home/head") ?>
+</head>
+<?php $this->load->view("home/content")?>
+<?php $this->load->view("home/isi")?>
+<?php $this->load->view("home/footer")?>
+</html>
