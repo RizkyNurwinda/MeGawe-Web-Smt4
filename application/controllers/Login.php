03bc25b62b799ec14d020b0cfc89afc50211ad6c
RizkyNurwinda <Rizkynurwinda@gmail.com>
2020-06-13 07:32:14 +0700

    app

---
+++ application/controllers/Login.php
@@ -0,0 +1,99 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('user');
+
+		if ($this->uri->segment(2) == 'logout') {
+			return;
+		}
+
+		if ($this->session->userdata('masuk')) {
+			redirect('home');
+		}
+	}
+
+	public function index()
+	{
+		$this->load->view('login/viewLogin');
+	}
+
+	public function cekDb($password)
+	{
+		$username = $this->input->post('username');
+		$result = $this->user->login($username, $password);
+		if ($result) {
+			$session = array();
+			foreach ($result as $value) {
+				$session = array(
+					'idUser' 	=> $value->idUser,
+					'username' 	=> $value->username,
+					'level'		=> $value->level
+				);
+				$this->session->set_userdata('masuk', $session);
+				$this->session->set_flashdata('dahMasuk', 'Berhasil Login');
+			}
+			return true;
+		} else {
+			$this->form_validation->set_message('cekDb', 'Login Gagal');
+			return false;
+		}
+	}
+
+	public function cekLogin()
+	{
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|callback_cekDb');
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('login/viewLogin');
+		} else {
+			redirect('home','refresh');
+		}
+	}
+
+	public function logout()
+	{
+		$this->session->unset_userdata('masuk');
+		// $this->session->sess_destroy();
+		$this->session->set_flashdata('dahKeluar', 'Anda Telah Logout');
+		redirect('login');
+	}
+
+	public function daftar($level)
+	{
+		$this->load->view('login/viewDaftar');
+	}
+
+	public function create($level)
+	{
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|is_unique[user.username]',
+			array(
+				'required' 		=> 'kolom %s tidak boleh kosong!!!!!!!',
+				'is_unique'		=> 'isi dari kolom %s sudah ada'
+			));
+		$this->form_validation->set_rules('password', 'Password', 'trim|required',
+			array(
+				'required' 		=> 'kolom %s tidak boleh kosong!!!!!!!',
+			));
+		$this->form_validation->set_rules('konfirmasiPassword', 'Konfirmasi Password', 'trim|required|matches[password]',
+			array(
+				'required' 		=> 'kolom %s tidak boleh kosong!!!!!!!',
+				'matches'		=> '%s tidak sesuai'
+			));
+		
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('login/viewDaftar');
+		} else {
+			$this->user->daftar($level);
+			redirect('login','refresh');
+		}
+	}
+
+}
+
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */
---
+++ application/views/login/viewLogin.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Login</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--===============================================================================================-->	
+		<?php include 'head.php'; ?>
+		<!--===============================================================================================-->	
+	</head>
+	<body style="background-color: #666666;">
+		
+		<div class="limiter">
+			<div class="container-login100">
+				<div class="wrap-login100">
+					<?php echo form_open('login/cekLogin', 'class="login100-form validate-form"'); ?>
+						<div class="container-fluid" id="pesan">
+							<?php if ($this->session->flashdata('dahKeluar')): ?>
+								<p class="alert alert-danger"><?= $this->session->flashdata('dahKeluar') ?></p>
+							<?php endif ?>
+							<?php if ($this->session->flashdata('acl')): ?>
+								<p class="alert alert-danger"><?= $this->session->flashdata('acl') ?></p>
+							<?php endif ?>
+						</div>
+						
+						<span class="login100-form-title p-b-43">
+							Login untuk Melanjutkan
+						</span>
+
+						<div class="text-danger text-center">
+							<?= validation_errors(); ?>
+						</div>
+
+						<div class="wrap-input100">
+							<input class="input100" type="text" name="username">
+							<span class="focus-input100"></span>
+							<span class="label-input100">Username</span>
+						</div>
+						<div class="wrap-input100">
+							<input class="input100" type="password" name="password">
+							<span class="focus-input100"></span>
+							<span class="label-input100">Password</span>
+						</div>
+						<div class="container-login100-form-btn">
+							<button type="submit" class="login100-form-btn">Login</button>
+						</div>
+						<p class="text-center">Or</p>
+						<div class="container-login100-form-btn">
+							<a href="#modal-id" data-toggle="modal" class="login100-form-btn" >Daftar</a>
+						</div>
+						<br>
+						<div class="container-login100-form-btn">
+							<a href="<?= base_url('index.php/home') ?>" class="login100-form-btn" style="background-color: #f00">Batal</a>
+						</div>
+
+					<!-- <?php form_close(); ?> -->
+					</form>
+
+					<div class="login100-more" style="background-image: url('<?= base_url('assets/images/1.png') ?>');">
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal fade" id="modal-id">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h4 class="modal-title">Belum Punya Akun?</h4>
+					</div>
+					<div class="modal-body text-center">
+						<h3>Daftar Sebagai</h3>
+						<br>
+						<a href="<?php echo base_url() ?>index.php/login/daftar/2" class="btn btn-primary">
+							Member
+						</a>
+						<a href="<?php echo base_url() ?>index.php/login/daftar/3" class="btn btn-primary">
+							Perusahaan
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!--===============================================================================================-->
+		<?php include 'foot.php'; ?>
+		<!--===============================================================================================-->
+	</body>
+</html>
