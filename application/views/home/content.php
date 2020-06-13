bb697df7eb7c799de243f080c55ad2512e1243c4
RizkyNurwinda <Rizkynurwinda@gmail.com>
2020-06-13 07:40:15 +0700

    add files via upload

---
+++ application/views/home/content.php
@@ -0,0 +1,214 @@
+<body>
+    <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+    
+    
+    <div class="site-navbar-wrap js-site-navbar bg-white">
+      
+      <div class="container">
+        <div class="site-navbar bg-light">
+          <div class="py-1">
+            <div class="row align-items-center">
+              <div class="col-2">
+                <h2 class="mb-0 site-logo"><strong class="font-weight-bold">MeGawe</strong></h2>
+              </div>
+              <div class="col-10">
+                <nav class="site-navigation text-right" role="navigation">
+                  <div class="container">
+                    <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+                    <ul class="site-menu js-clone-nav d-none d-lg-block">
+                      <li><a href="<?php echo base_url('home');?>">Home</a></li>
+                      <li><a href="category.html">Akun</a></li>
+                      <li><a href="contact.html">Tentang Kami</a></li>
+                      <li><a href="contact.html">Kontak</a></li>
+                      <li><a href="new-post.html">Tambah Pekerjaan</a></li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<div style="height: 113px;"></div>
+  
+    <div class="site-blocks-cover overlay" style="background-image: url('asset/images/image2.jpg');" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12" data-aos="fade">
+            <form action="#">
+              <div class="row mb-3">
+                <div class="col-md-9">
+                  <div class="row">
+                    <div class="col-md-6 mb-3 mb-md-0">
+                      <input type="text" class="mr-3 form-control border-0 px-4" placeholder="jenis pekerjaan, nama perusahaan">
+                    </div>
+                    <div class="col-md-6 mb-3 mb-md-0">
+                      <div class="input-wrap">
+                        <span class="icon icon-room"></span>
+                      <input type="text" class="form-control form-control-block search-input  border-0 px-4" id="autocomplete" placeholder="lokasi" onFocus="geolocate()">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <input type="submit" class="btn btn-search btn-primary btn-block" value="Search">
+                </div>
+              </div>
+              <div class="row">
+              </div>
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!--Bagian Recent Job-->
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row justify-content-start text-left mb-5">
+          <div class="col-md-6" data-aos="fade">
+            <h2 class="font-weight-bold text-black">Temukan Pekerjaan</h2>
+          </div>
+        </div>
+          <div class="col-md-4 block-16" data-aos="fade-up" data-aos-delay="200">
+            <div class="d-flex mb-0">
+            </div>
+            <div class="col-xs-100 col-sm-5"> 
+              <div class="border rounded p-4 bg-white">
+                <h2 class="h5">Pramusaji</h2>
+                
+                  <span class="d-block"><span class="icon-suitcase"></span> Restoran Apung</span>
+                  <span class="d-block"><span class="icon-room"></span> Jakarta Pusat</span>
+                  <span class="d-block"><span class="icon-money mr-1"></span> 500000 &mdash; 70000</span>
+                </p>
+                <p class="mb-0">Dibutuhkan pramusaji yang dapat melayani pelanggan dengan baik. </p>
+              </br>
+                <a href="carikerja.php">
+                  <div class="col-md-8">
+                    <input type="submit" class="btn btn-primary btn-block" value="Lebih Lanjut"></a>
+                  </div>
+              </div>
+
+              <div class="border rounded p-4 bg-white">
+                <h2 class="h5">Akuntan</h2>
+              
+                  <span class="d-block"><span class="icon-suitcase"></span> PT Makmur</span>
+                  <span class="d-block"><span class="icon-room"></span> Bandung</span>
+                  <span class="d-block"><span class="icon-money mr-1"></span> 1000000 &mdash; 2000000</span>
+                </p>
+                <p class="mb-0">Dibutuhkan akuntan yang memahami akuntansi dengan baik.</p>
+                </br>
+                <a href="carikerja.php">
+                  <div class="col-md-8">
+                    <input type="submit" class="btn btn-primary btn-block" value="Lebih Lanjut"></a>
+                  </div>
+              </div>
+
+              <div class="border rounded p-4 bg-white">
+                <h2 class="h5">Sekretaris</h2>
+                
+                  <span class="d-block"><span class="icon-suitcase"></span> PT UI Corporate</span>
+                  <span class="d-block"><span class="icon-room"></span> Jakarta Selatan</span>
+                  <span class="d-block"><span class="icon-money mr-1"></span> 2000000 &mdash; 4000000</span>
+                </p>
+                <p class="mb-0">Dibutuhkan seorang sekretaris yang terampil, dan cekatan.</p>
+                </br>
+                <a href="carikerja.php">
+                  <div class="col-md-8">
+                    <input type="submit" class="btn btn-primary btn-block" value="Lebih Lanjut"></a>
+                  </div>
+              </div>
+
+              <div class="border rounded p-4 bg-white">
+                <h2 class="h5">Desain Grafis</h2>
+               
+                  <span class="d-block"><span class="icon-suitcase"></span> SMILE </span>
+                  <span class="d-block"><span class="icon-room"></span> Yogyakarta</span>
+                  <span class="d-block"><span class="icon-money mr-1"></span> 1000000 &mdash; 2000000</span>
+                </p>
+                <p class="mb-0">Dibutukan seorang desain grafis yang dapat membuat desain berkualitas.</p>
+              </br>
+                <a href="carikerja.php">
+                  <div class="col-md-8">
+                    <input type="submit" class="btn btn-primary btn-block" value="Lebih Lanjut"></a>
+                  </div>
+              </div>
+
+                <div class="border rounded p-4 bg-white">
+                <h2 class="h5">Guru PAUD</h2>
+               
+                  <span class="d-block"><span class="icon-suitcase"></span> Asosiasi Rumah Kita</span>
+                  <span class="d-block"><span class="icon-room"></span> Magelang</span>
+                  <span class="d-block"><span class="icon-money mr-1"></span> 1000000 &mdash; 2000000</span>
+                </p>
+                <p class="mb-0">Dibutukan guru PAUD yang dapat membantu proses belajar siswa.</p>
+              </br>
+                <a href="carikerja.php">
+                  <div class="col-md-8">
+                    <input type="submit" class="btn btn-primary btn-block" value="Lebih Lanjut"></a>
+                  </div>
+              </div>
+
+              <div class="border rounded p-4 bg-white">
+                <h2 class="h5">Desain Grafis</h2>
+               
+                  <span class="d-block"><span class="icon-suitcase"></span> ABC Ok</span>
+                  <span class="d-block"><span class="icon-room"></span> Majalengka</span>
+                  <span class="d-block"><span class="icon-money mr-1"></span> 1000000 &mdash; 2000000</span>
+                </p>
+                <p class="mb-0">Dibutukan seorang desain grafis yang dapat membuat desain yang berkualitas.</p>
+              </br>
+                <a href="carikerja.php">
+                  <div class="col-md-8">
+                    <input type="submit" class="btn btn-primary btn-block" value="Lebih Lanjut"></a>
+                  </div>
+              </div>
+            </div>
+        </div>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+      </div>
+    </div>
+     <div class="site-section" data-aos="fade">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 mb-5 mb-md-0">
+            
+              <div class="img-border">
+                  <img src="asset/images/image1.jpg" alt="Image" class="img-fluid rounded">
+                </a>
+              </div>
+            
+          </div>
+          <div class="col-md-5 ml-auto">
+            <div class="text-left mb-5 section-heading">
+              <h2>Tentang Kami</h2>
+            </div>
+
+            <p class="mb-4 h5 font-italic lineheight1-5">&ldquo;MeGawe adalah sebuah website yang menyediakan informasi lowongan pekerjaan dari berbagai jenis pekerjaan seperti kesehatan, pariwisata, transportasi, dan masih banyak lagi. Selain itu MeGawe juga dapat memasukan lowongan pekerjaan.&rdquo;</p>
+            
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  
---
+++ application/views/home/head.php
@@ -0,0 +1,21 @@
+<!-- Mobile Specific Meta -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Favicon-->
+<link rel="shortcut icon" href="<?= base_url('assets/home/img/fav.ico') ?>">
+<!-- meta character set -->
+<meta charset="UTF-8">
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+<!-- ====================== CSS ======================= -->
+<!-- JSGrid -->
+<link rel="stylesheet" href="<?= base_url()?>assets/jsgrid/jsgrid.min.css">
+<link rel="stylesheet" href="<?= base_url()?>assets/jsgrid/jsgrid-theme.min.css">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/datatables/datatables.min.css') ?>"/>
+<link rel="stylesheet" href="<?= base_url('assets/home/css/linearicons.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/home/css/font-awesome.min.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/home/css/bootstrap.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/home/css/magnific-popup.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/home/css/nice-select.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/home/css/animate.min.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/home/css/owl.carousel.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/home/css/main.css') ?>">
